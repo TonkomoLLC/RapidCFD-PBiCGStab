f5de45bd2ec5adb210bba0892b2123e34b010eb7
Eric Daymo <eadaymo@tonkomo.com>
2018-10-01 17:16:03 -0700

    Initial Commit

---
+++ PBiCGStab_SharedLib/PCGCache.C
@@ -0,0 +1,21 @@
+#include "PCGCache.H"
+
+namespace Foam
+{
+    PtrList<scalargpuField> PCGCache::pACache(1);
+//    PtrList<scalargpuField> PCGCache::wACache(1);
+    PtrList<scalargpuField> PCGCache::rACache(1);
+
+//    PtrList<scalargpuField> PCGCache::pTCache(1);
+//    PtrList<scalargpuField> PCGCache::wTCache(1);
+//    PtrList<scalargpuField> PCGCache::rTCache(1);
+
+    PtrList<scalargpuField> PCGCache::yACache(1);
+    PtrList<scalargpuField> PCGCache::AyACache(1);
+    PtrList<scalargpuField> PCGCache::sACache(1);
+    PtrList<scalargpuField> PCGCache::zACache(1);
+    PtrList<scalargpuField> PCGCache::tACache(1);
+    PtrList<scalargpuField> PCGCache::result1Cache(1);
+
+
+}
